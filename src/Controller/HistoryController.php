1d916d4eead056bc3d3a0297b32003da4bb06de0
Lilian Hammache <fayzerdev@gmail.com>
2025-06-07 12:35:21 +0200

    ♻️  Refactor: Comment out unused show, edit, and delete methods in HistoryController

--- src/Controller/HistoryController.php
+++ src/Controller/HistoryController.php
@@ -73,42 +73,42 @@ final class HistoryController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}', name: 'app_history_show', methods: ['GET'])]
-    public function show(History $history): Response
-    {
-        return $this->render('history/show.html.twig', [
-            'history' => $history,
-        ]);
-    }
+    // #[Route('/{id}', name: 'app_history_show', methods: ['GET'])]
+    // public function show(History $history): Response
+    // {
+    //     return $this->render('history/show.html.twig', [
+    //         'history' => $history,
+    //     ]);
+    // }
 
-    #[Route('/{id}/edit', name: 'app_history_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, History $history, EntityManagerInterface $entityManager): Response
-    {
-        $form = $this->createForm(HistoryForm::class, $history);
-        $form->handleRequest($request);
+    // #[Route('/{id}/edit', name: 'app_history_edit', methods: ['GET', 'POST'])]
+    // public function edit(Request $request, History $history, EntityManagerInterface $entityManager): Response
+    // {
+    //     $form = $this->createForm(HistoryForm::class, $history);
+    //     $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->flush();
+    //     if ($form->isSubmitted() && $form->isValid()) {
+    //         $entityManager->flush();
 
-            return $this->redirectToRoute('app_history_index', [], Response::HTTP_SEE_OTHER);
-        }
+    //         return $this->redirectToRoute('app_history_index', [], Response::HTTP_SEE_OTHER);
+    //     }
 
-        return $this->render('history/edit.html.twig', [
-            'history' => $history,
-            'form' => $form,
-        ]);
-    }
+    //     return $this->render('history/edit.html.twig', [
+    //         'history' => $history,
+    //         'form' => $form,
+    //     ]);
+    // }
 
-    #[Route('/{id}', name: 'app_history_delete', methods: ['POST'])]
-    public function delete(Request $request, History $history, EntityManagerInterface $entityManager): Response
-    {
-        if ($this->isCsrfTokenValid('delete'.$history->getId(), $request->getPayload()->getString('_token'))) {
-            $entityManager->remove($history);
-            $entityManager->flush();
-        }
+    // #[Route('/{id}', name: 'app_history_delete', methods: ['POST'])]
+    // public function delete(Request $request, History $history, EntityManagerInterface $entityManager): Response
+    // {
+    //     if ($this->isCsrfTokenValid('delete'.$history->getId(), $request->getPayload()->getString('_token'))) {
+    //         $entityManager->remove($history);
+    //         $entityManager->flush();
+    //     }
 
-        return $this->redirectToRoute('app_history_index', [], Response::HTTP_SEE_OTHER);
-    }
+    //     return $this->redirectToRoute('app_history_index', [], Response::HTTP_SEE_OTHER);
+    // }
     #[Route('/launch/{id}', name: 'app_history_launch', methods: ['GET'])]
     public function launch($id, EntityManagerInterface $entityManager): Response
     {
