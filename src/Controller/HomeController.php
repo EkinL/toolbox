365ac9b3acfc5bc4482b4b25b1a5e64f08d8f6ae
Lilian Hammache <fayzerdev@gmail.com>
2025-06-16 21:41:39 +0200

    🐛 Fix: Add missing newline at the end of HomeController.php

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -27,5 +27,6 @@ final class HomeController extends AbstractController
         return $this->render('home/index.html.twig', [
             'toolboxes' => $toolboxes,
         ]);
+        
     }
 }
